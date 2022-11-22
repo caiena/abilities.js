91bc203331be0ebd00d95716f1cc3fef6ea5d035
Pedro de Assis <pedro@caiena.net>
2022-11-22 15:08:20 -0300

    v0.1.1

--- test/setup.js
+++ test/setup.js
@@ -14,9 +14,6 @@ import chaiSubset from 'chai-subset'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 
-// debugging
-import pry from 'pryjs'
-
 chai.use(chaiAsPromised)
 chai.use(chaiChange)
 chai.use(chaiDatetime)
@@ -31,17 +28,3 @@ global.expect = chai.expect
 global.sinon = sinon
 
 // checkout http://sinonjs.org/releases/v4.4.2/sandbox/
-
-
-// exporting pryjs
-// to debug, call a `eval(pry.it)` anywhere on the code
-global.pry = pry
-
-
-// booting the support app
-// import './support/app/boot'
-
-
-// exporting the lib globally
-// import _ from '../src/index'
-// global._ = _
